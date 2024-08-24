c3716d108714edf0abd18c91f4efd0a9dfb34c1a
Fausto Obama <fobama@outlook.com>
2024-08-24 21:47:03 +0200

    First commit

---
+++ demo/src/test/java/com/MainTestController.java
@@ -0,0 +1,13 @@
+package com;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTestController {
+
+    @Test
+    void test(){
+        assertEquals("a", "a");
+    }
+}
